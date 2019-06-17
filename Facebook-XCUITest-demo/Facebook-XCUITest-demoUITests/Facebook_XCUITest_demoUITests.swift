a98ae76a83035fd50e8dcd3655c1a7a8e421539a
Eugene Berezin <berezin.nlp@gmail.com>
2019-06-16 22:30:23 -0700

    clean up

--- Facebook-XCUITest-demo/Facebook-XCUITest-demoUITests/Facebook_XCUITest_demoUITests.swift
+++ Facebook-XCUITest-demo/Facebook-XCUITest-demoUITests/Facebook_XCUITest_demoUITests.swift
@@ -28,17 +28,17 @@ class Facebook_XCUITest_demoUITests: XCTestCase {
         //TODO: Create page object model and refactor the code
         
         facebookApp.launch()
-        let createAccount = facebookApp /*@START_MENU_TOKEN@*/.buttons["sign-up-button"]/*[[".otherElements[\"login-main\"].buttons[\"sign-up-button\"]",".buttons[\"sign-up-button\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        let getStartedButton = facebookApp/*@START_MENU_TOKEN@*/.buttons["get-started-button-welcome"]/*[[".scrollViews",".buttons[\"Get Started\"]",".buttons[\"get-started-button-welcome\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
-        let firstNameTextFiled = facebookApp.scrollViews.otherElements/*@START_MENU_TOKEN@*/.textFields["first-name-field"]/*[[".textFields[\"First name\"]",".textFields[\"first-name-field\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        let lastNameTextField = facebookApp.scrollViews.otherElements/*@START_MENU_TOKEN@*/.textFields["last-name-field"]/*[[".textFields[\"Last name\"]",".textFields[\"last-name-field\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let createAccount = facebookApp .buttons["sign-up-button"]
+        let getStartedButton = facebookApp.buttons["get-started-button-welcome"]
+        let firstNameTextFiled = facebookApp.scrollViews.otherElements.textFields["first-name-field"]
+        let lastNameTextField = facebookApp.scrollViews.otherElements.textFields["last-name-field"]
         let maleGender = facebookApp.scrollViews.children(matching: .other).element(boundBy: 1).children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element(boundBy: 0).children(matching: .other).element
-        let nextButton =  facebookApp/*@START_MENU_TOKEN@*/.buttons["Next"]/*[[".scrollViews.buttons[\"Next\"]",".buttons[\"Next\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        let registerByEmailButton = facebookApp/*@START_MENU_TOKEN@*/.buttons["change-contact-point-type-button"]/*[[".scrollViews",".buttons[\"Use your email address\"]",".buttons[\"change-contact-point-type-button\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
-        let emailTextField = facebookApp.scrollViews.otherElements/*@START_MENU_TOKEN@*/.textFields["contact-point-email-field"]/*[[".textFields[\"Enter your email address\"]",".textFields[\"contact-point-email-field\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let nextButton =  facebookApp.buttons["Next"]
+        let registerByEmailButton = facebookApp.buttons["change-contact-point-type-button"]
+        let emailTextField = facebookApp.scrollViews.otherElements.textFields["contact-point-email-field"]
         let goButton = facebookApp.buttons["Go"]
-        let passwordTextField = facebookApp.scrollViews.otherElements/*@START_MENU_TOKEN@*/.textFields["password-field"]/*[[".textFields[\"Password\"]",".textFields[\"password-field\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        let signUp = facebookApp/*@START_MENU_TOKEN@*/.buttons["get-started-button"]/*[[".scrollViews",".buttons[\"Sign Up\"]",".buttons[\"get-started-button\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+        let passwordTextField = facebookApp.scrollViews.otherElements.textFields["password-field"]
+        let signUp = facebookApp.buttons["get-started-button"]
         let logOutButton = facebookApp.navigationBars["Confirm Your Account"].buttons["Log out"]
         
         
